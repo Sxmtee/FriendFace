e16cff0653e56ff502fd3ad0a20068413720244f
sxmtee <somtoo76@gmail.com>
2024-10-23 16:06:14 +0100

    Friend Face project complete

--- FriendFace/FriendFaceApp.swift
+++ FriendFace/FriendFaceApp.swift
@@ -5,6 +5,7 @@
 //  Created by mac on 22/10/2024.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct FriendFaceApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
